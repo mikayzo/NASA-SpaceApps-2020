a3a543851ce3ae3d5158e0f1e25966ee136325e1
Dziugas Mikalkenas <dziugas.mikalkenas.ext@home24.de>
2020-10-04 00:32:50 +0300

    Add camera_service

--- camera_service/index.js
+++ camera_service/index.js
@@ -1,46 +1,26 @@
-//const fs = require('fs');
-//const YAML = require('yaml')
 const express = require('express');
 const bodyParser = require('body-parser')
+
 const app = express()
 const port = 80
-
 const east = Math.PI / 2;
 const south = Math.PI;
 const interval = Math.PI / 8;
-const url = "http://" + process.env.HOSTNAME;
 
 app.use(bodyParser.json())
-app.use(express.static(__dirname + '/public'));
-app.set('views', __dirname + '/public/');
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
 
-app.post('/take-picture', (req, res) => {
+app.post('/takepic', (req, res) => {
     if (between(req.body.alpha, east, east + interval)) {
-        res.send(url + "/picture/0")
+        res.send("https://nasa2020-canyouhearmenow-mars-bucket.s3.eu-north-1.amazonaws.com/0.jpg")
     } else if (between(req.body.alpha, east + interval, east + 2 * interval)) {
-        res.send(url + "/picture/1")
+        res.send("https://nasa2020-canyouhearmenow-mars-bucket.s3.eu-north-1.amazonaws.com/1.jpg")
     } else if (between(req.body.alpha, east + 2 * interval, east + 3 * interval)) {
-        res.send(url + "/picture/2")
+        res.send("https://nasa2020-canyouhearmenow-mars-bucket.s3.eu-north-1.amazonaws.com/2.jpg")
     } else if (between(req.body.alpha, east + 3 * interval, south)) {
-        res.send(url + "/picture/3")
+        res.send("https://nasa2020-canyouhearmenow-mars-bucket.s3.eu-north-1.amazonaws.com/3.jpg")
     } else {
         res.send("Try turning another way :)");
     }
-})
-
-app.get('/picture/0', (req, res) => {
-    res.render('pic.html', { image: "0.jpg" });
-})
-app.get('/picture/1', (req, res) => {
-    res.render('pic.html', { image: "1.jpg" });
-})
-app.get('/picture/2', (req, res) => {
-    res.render('pic.html', { image: "2.jpg" });
-})
-app.get('/picture/3', (req, res) => {
-    res.render('pic.html', { image: "3.jpg" });
 })
 
 app.listen(port, "", () => {
